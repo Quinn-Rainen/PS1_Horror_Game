0bf748d3f0be83d669aee4ef16cf86b01a4e3a68
jdenckla <jdenckla@uoregon.edu>
2023-04-30 15:07:45 -0700

    Added sprint (Lshift) and crouch (Lctrl)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 10f;
+    public float movementSpeed = 10f;
     public float gravity = -9.81f;
+    public float vertVelocity = 0;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    public bool isSprinting = false;
+    public float sprintSpeedMult;
 
+    public bool isCrouching = false;
+    public float crouchingMultiplier;
+
+    public float standingHeight = 1.8f;
+    public float crouchingHeight = 1.25f;
 
     Vector3 velocity;
     bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         //used for gravity and falling as the Character Controller is a different way of implementing moving and doesn't have gravity as a trigger
         //might try later to implement it with a rigid body instead but this supposedly allows us to implement crouching easier so
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float forward = Input.GetAxis("Vertical");
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -30,17 +47,44 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+        } else {
+            isCrouching = false;
+        }
 
-         float horizontal = Input.GetAxis("Horizontal");
-         float forward = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift) && isCrouching == false)
+        {
+            isSprinting = true;
+        } else {
+            isSprinting = false;
+        }
+
+
+         
 
         //calculate the movement then apply it
         Vector3 move = transform.right * horizontal + transform.forward * forward;
-        controller.Move(move * speed * Time.deltaTime);
+        if (isCrouching == true)
+        {
+            controller.height = crouchingHeight;
+            move *= crouchingMultiplier;
+        } else {
+            controller.height = standingHeight;
+        }
+
+        if (isSprinting == true)
+        {
+            move *= sprintSpeedMult;
+        }
 
         // calculate the effect and apply gravity to player
         velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(move * movementSpeed * Time.deltaTime);
         controller.Move(velocity * Time.deltaTime);
+
 
     }
 
