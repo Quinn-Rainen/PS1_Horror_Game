938303571a4518172d5a0a3107508e617c75a7f5
Quinn-Rainen <q.rainen@gmail.com>
2023-05-21 00:13:26 -0700

    New and Improved Audio Clips

--- Assets/Scripts/ScientistVoice.cs
+++ Assets/Scripts/ScientistVoice.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ScientistVoice : MonoBehaviour
 {
     private AudioSource scienVoice;
+    private AudioSource scienVoice2;
+
    // private bool hasPlayed;
 
     void Start()
     {
         scienVoice = GetComponent<AudioSource>();
         scienVoice.Stop();
+
+        scienVoice2 = GetComponent<AudioSource>();
+        scienVoice2.Stop();
         //hasPlayed = false;
     }
 
@@ -21,5 +26,13 @@ public class ScientistVoice : MonoBehaviour
             scienVoice.Play();
             // hasPlayed = true;
         }
+    }
+
+    void OnTriggerExit(Collider other){
+        if (other.CompareTag("Player") )//&& !hasPlayed)
+        {
+            scienVoice2.Play();
+            // hasPlayed = true;
+        }      
     }
 }
