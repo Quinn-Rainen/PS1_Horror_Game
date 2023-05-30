96dfa587ed3cd7c7422624f0c8e762acfa69e38f
zfgo <89813559+zfgo@users.noreply.github.com>
2023-05-30 14:45:59 -0700

    add main menu music

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -6,14 +6,18 @@ using UnityEngine.UI;
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] Button _newGame;
+    AudioSource mm_AudioSource;
 
     void Start()
     {
         _newGame.onClick.AddListener(StartNewGame);
+        mm_AudioSource = GetComponent<AudioSource>();
+        mm_AudioSource.Play();
     }
 
     private void StartNewGame()
     {
+        mm_AudioSource.Stop();
         ScenesManager.Instance.LoadNewGame();
     }
 }
