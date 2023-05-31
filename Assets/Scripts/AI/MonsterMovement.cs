a163fdefcd10f4aea5b45f046ae163041b9704ec
Quinn-Rainen <q.rainen@gmail.com>
2023-05-31 02:36:13 -0700

    Main Scene
    Forever thing

--- Assets/Scripts/AI/MonsterMovement.cs
+++ Assets/Scripts/AI/MonsterMovement.cs
@@ -24,17 +24,17 @@ public class MonsterMovement : MonoBehaviour
 
     IEnumerator MoveTowardsPlayer()
     {
+        // No matter if the player is moving or if the monster is close enough it deletes it self after moving towards the player
         float elapsedTime = 0f;
         float maxDuration = 3.5f; 
 
-        // No matter if the player is moving or if the monster is close enough it deletes it self after moving towards the player
-        while (elapsedTime < maxDuration && Vector3.Distance(jumpscareObject.transform.position, playerTransform.position) > 0.1f)
+        while (elapsedTime < maxDuration)
         {
-            jumpscareObject.transform.position = Vector3.Lerp(jumpscareObject.transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+            Vector3 targetPosition = playerTransform.position + Vector3.up * 10f;
+            jumpscareObject.transform.position = Vector3.Lerp(jumpscareObject.transform.position, targetPosition, moveSpeed * Time.deltaTime);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-
 
         Destroy(jumpscareObject);
         Destroy(gameObject);
