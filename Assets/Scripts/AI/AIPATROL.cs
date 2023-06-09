36e7dac592bc0ed56a0ba60debcaa8508c6a2099
Quinn-Rainen <q.rainen@gmail.com>
2023-06-08 18:56:32 -0700

    Finishing Touches

--- Assets/Scripts/AI/AIPATROL.cs
+++ Assets/Scripts/AI/AIPATROL.cs
@@ -14,40 +14,29 @@ OUTDATED SCRIPT FOR STORING PURPOSES
  */
 public class AIPATROL : MonoBehaviour
 {
-
+    // Dumbed down version of the AI movement script that just patrols
     public NavMeshAgent agent;
-    public Transform player;
-    public LayerMask Ground, Player;
-    //private Animator animator;
 
-    public bool isMoving = false;
-    public bool isAttacking = false;
-//----------------AI Movement Variables---------------------
-    public float sightRange, attackRange;
-    public float patrolSpeed = 2f;
-    public float chaseSpeed = 5f;
+    //----------------AI Movement Variables---------------------
+    public float patrolSpeed = 6f;
     public float rotationSpeed = 5f;
+
     private AIState currentState;
     private Vector3 patrolPoint;
     private bool walkPointSet;
-//----------------------------------------------------------------
-
-
-    // audio sources
+    private bool alreadyAttacked;
 
     private enum AIState
     {
-        Patrol,Chase,Attack
+        Patrol
     }
-
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.Warp(transform.position);
         //Moves Erins model up 
-        agent.baseOffset = transform.position.y * .04f;
-
+        agent.baseOffset = transform.position.y * 1f;
     }
 
     private void Start()
@@ -58,122 +47,48 @@ public class AIPATROL : MonoBehaviour
 
     private void Update()
     {
-        switch (currentState)
+        transform.localEulerAngles = Vector3.forward;
+
+        if (currentState == AIState.Patrol && !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
         {
-            case AIState.Patrol:
-                Patrol();
-                break;
-            // case AIState.Chase:
-            //     Chase();
-            //     break;
-            // case AIState.Attack:
-            //     Attack();
-            //     break;
+            StartCoroutine(Patrol());
         }
-
-
-        CheckTransitions();
     }
 
-   
-
-    private void Patrol()
+    private IEnumerator Patrol()
     {
+        // Debug.Log("Outer Patrol");
+
         if (!walkPointSet || Vector3.Distance(transform.position, patrolPoint) < 1f)
         {
+            // Debug.Log("Inner Patrol");
             SetNextPatrolPoint();
         }
 
         agent.speed = patrolSpeed;
-        // float speed2 = agent.velocity.magnitude;
-        // animator.SetFloat("Speed", speed2);
         agent.SetDestination(patrolPoint);
-        // Debug.Log(isAttacking);
+        walkPointSet = false; // Reset the walkPointSet flag
+
+        // Wait until the agent reaches the destination
+        yield return new WaitUntil(() => agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending);
+
+        // Wait for a delay before setting the next patrol point
+        float delay = 2f; // Adjust the delay duration as needed
+        yield return new WaitForSeconds(delay);
+
+        // Set the next patrol point
+        StartCoroutine(Patrol());
     }
 
     private void SetNextPatrolPoint()
     {
-        float randomZ = Random.Range(-20f, 20f);
-        float randomX = Random.Range(-20f, 20f);
+        // Debug.Log("Inside Set Patrol");
+
+        // potentially make this an adjustable slider in the editor for final version
+        float randomZ = Random.Range(-60f, 60f);
+        float randomX = Random.Range(-60f, 60f);
 
         patrolPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
         walkPointSet = true;
-    }
-
-    // private void Chase()
-    // {
-
-    //     //Debug.Log("CHASING");
-
-    //     agent.speed = chaseSpeed;
-
-    //     // Calculate the direction from the AI to the player
-    //     Vector3 chaseDirection = player.position - transform.position;
-
-    //     Vector3 targetPosition = transform.position + chaseDirection;
-
-    //     agent.SetDestination(targetPosition);
-
-    //     Quaternion targetRotation = Quaternion.LookRotation(chaseDirection);
-    //     transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-    // }
-
-    // private void Attack()
-    // {
-    //     //Debug.Log("Attacking Now");
-    //     agent.SetDestination(transform.position);
-
-    //     Quaternion targetRotation = Quaternion.LookRotation(player.position - transform.position);
-    //     transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        
-
-    //     if (!alreadyAttacked)
-    //     {
-    //         // Future Implementaion of the Jumpscare  code here
-    //         // isAttacking = true;
-    //         // animator.SetBool("isAttacking", isAttacking);
-    //         alreadyAttacked = true;
-    //         Invoke(nameof(ResetAttack), timeBetweenAttacks);
-    //     }
-    // }
-
-    private void ResetAttack()
-    {
-        Debug.Log("RESETATTACK");
-
-        // isAttacking = false;
-        currentState = AIState.Patrol; 
-    }
-
-    private void CheckTransitions()
-    {
-        Vector3 aiPosition = agent.nextPosition;
-        Vector3 playerPosition = player.position;
-
-        float distanceToPlayer = (playerPosition - aiPosition).magnitude;
-
-        //Debug.Log("Distance: " + distanceToPlayer);
-        if (distanceToPlayer <= attackRange && IsFacingPlayer())
-        {
-            // Player within attack range
-            currentState = AIState.Attack;
-        }
-        else if (distanceToPlayer > attackRange && distanceToPlayer <= sightRange)
-        {
-            // Player within sight range but outside attack range
-            currentState = AIState.Chase;
-        }
-        else
-        {
-            currentState = AIState.Patrol;
-        }
-    }
-
-
-    private bool IsFacingPlayer()
-    {
-        Vector3 directionToPlayer = player.position - transform.position;
-        float angle = Vector3.Angle(transform.forward, directionToPlayer);
-        return angle < 60f;
     }
 }
